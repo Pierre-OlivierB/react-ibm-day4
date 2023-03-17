abb9dc8843cb2764edb28325837669cd585d9841
unknown <baudinopierreolivier@gmail.com>
2023-03-17 10:44:18 +0100

    add simple front content

--- level_one.jsx
+++ level_one.jsx
@@ -1,14 +1,18 @@
 // Création de composant en créant des classes
 function App() {
-  const song = "Chachacha";
-  const numberOfVotes = 149;
-  const Button = <button>Vote</button>;
-  const disabledButton = <button disabled>Vote</button>;
+  const title_page = <h1>Composants</h1>;
+  const form_comp = (
+    <form className="container d-flex justify-content-between half">
+      <input type="password" placeholder="Password" />
+      <button className="btn btn-primary">Valider</button>
+    </form>
+  );
 
   return (
     // Ceci est ce qui va s'afficher.
     <div>
-      <p>Test</p>
+      {title_page}
+      {form_comp}
     </div>
   );
 }
